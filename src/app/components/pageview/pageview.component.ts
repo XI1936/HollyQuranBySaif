0f1dcdfdb023991fbdbe4e6a193ac850e003d55f
Shaikh Saifuddin <shaikh.saifuddin@xebia.com>
2022-06-30 13:57:17 +0530

    folder structure change

---
+++ src/app/components/pageview/pageview.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pageview',
+  templateUrl: './pageview.component.html',
+  styleUrls: ['./pageview.component.css']
+})
+export class PageviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
